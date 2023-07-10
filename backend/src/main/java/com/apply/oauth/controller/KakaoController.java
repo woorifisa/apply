4842cc82b3f50b85bc68ed2d87c921cf66b60c00
kongtaesic <xotlr333@naver.com>
2023-07-10 23:23:22 +0900

    feat(oauth): 인가 코드 발급
    클라이언트에서 로그인 후 서버로 인가 코드가 발급된다.
    발급된 인가코드를 확인하도록 구현했다.

---
+++ backend/src/main/java/com/apply/oauth/controller/KakaoController.java
@@ -0,0 +1,25 @@
+package com.apply.oauth.controller;
+
+import com.apply.oauth.service.KakaoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/app/oauth/kakao")
+@Slf4j
+public class KakaoController {
+    private final KakaoService kakaoService;
+
+    public KakaoController(KakaoService kakaoService){
+         this.kakaoService = kakaoService;
+    }
+
+    @GetMapping
+    public String kakaoOauth(@RequestParam("code") String code) {
+        log.info("인가 코드를 이용하여 토큰을 받습니다. code: " + code);
+        System.out.println("code: " + code);
+        
+        return "okay";
+    }
+
+}
