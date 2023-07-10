b766a26c747e3c5e0bcde3bb24f111f450460fa1
han0224 <dbn0224@gmail.com>
2023-07-10 17:48:37 +0900

    feat: 문자열 파일 생성
    Header와 Footer와 관련된 문자열과 link 등 필요한 정보들을 담고 있는 파일 생성

---
+++ frontend/src/constants/String.js
@@ -0,0 +1,52 @@
+export const HEADER = {
+  LOGO: '우리 FISA',
+  CONTENTS: [
+    {
+      TEXT: '교육과정',
+      LINK: '/'
+    },
+    {
+      TEXT: '지원하기',
+      LINK: '/'
+    },
+    {
+      TEXT: '문의하기(FnQ)',
+      LINK: '/'
+    }
+  ],
+  LOGIN: '로그인'
+};
+
+export const FOOTER = {
+  LOGO: '우리 FISA',
+  CONTACT: {
+    TITLE: 'contact',
+    CONTENTS: [
+      {
+        TYPE: 'phone',
+        VALUE: '00-0000-0000'
+      },
+      {
+        TYPE: 'email',
+        VALUE: '0000@000.000'
+      }
+    ]
+  },
+  LINKS: {
+    TITLE: 'Quick Links',
+    CONTENTS: [
+      {
+        TITLE: '우리 FISA 소개',
+        LINK: ''
+      },
+      {
+        TITLE: '교육과정 소개',
+        LINK: ''
+      },
+      {
+        TITLE: '문의처',
+        LINK: ''
+      }
+    ]
+  }
+};
