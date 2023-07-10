8cf2fe491fb9a91fe2d9069372e9b1ea8e85b847
han0224 <dbn0224@gmail.com>
2023-07-10 20:51:33 +0900

    fix: header text 줄바꿈 문제 수정
    반응형으로 제작하여 1024보다 작을 경우 햄버거 버튼으로 대체
    햄버거버튼 svg는 따로 컴포넌트로 분리

---
+++ frontend/src/components/icons/HamburgerMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    width="32px"
+    height="32px"
+    viewBox="-2.4 -2.4 28.80 28.80"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke="#CCCCCC"
+      stroke-width="0.43200000000000005"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <path d="M4 18L20 18" stroke="#000000" stroke-width="2.4" stroke-linecap="round"></path>
+      <path d="M4 12L20 12" stroke="#000000" stroke-width="2.4" stroke-linecap="round"></path>
+      <path d="M4 6L20 6" stroke="#000000" stroke-width="2.4" stroke-linecap="round"></path>
+    </g>
+  </svg>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- frontend/src/layouts/DefaultHeader.vue
+++ frontend/src/layouts/DefaultHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-white flex flex-row justify-between py-4 px-52 drop-shadow-md h-16 items-center text-center text-lg"
+    class="bg-white flex flex-row justify-between py-4 px-52 drop-shadow-md h-16 items-center text-center text-lg text-black"
   >
     <div>
       <router-link to="/" class="font-bold text-2xl text-blue-600">
@@ -8,21 +8,62 @@
         {{ HEADER.LOGO }}
       </router-link>
     </div>
-    <ul class="flex flex-row gap-4 items-center">
-      <li v-for="(item, index) in HEADER.CONTENTS" :key="index">
-        <router-link :to="item.LINK">{{ item.TEXT }}</router-link>
-      </li>
-      <li>
-        <button class="bg-blue-600 text-white border rounded-xl px-3 py-1 active:bg-blue-800">
-          {{ HEADER.LOGIN }}
-        </button>
-      </li>
-    </ul>
+    <div class="menu">
+      <ul class="flex gap-4 items-center">
+        <li v-for="(item, index) in HEADER.CONTENTS" :key="index">
+          <router-link :to="item.LINK">{{ item.TEXT }}</router-link>
+        </li>
+        <li>
+          <button class="bg-blue-600 text-white border rounded-xl px-3 py-1 active:bg-blue-800">
+            {{ HEADER.LOGIN }}
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div class="hamburger_menu">
+      <button @click="openMenu">
+        <HamburgerMenu />
+      </button>
+      <ul v-if="viewMenu" class="items">
+        <li v-for="(item, index) in HEADER.CONTENTS" :key="index">
+          <router-link :to="item.LINK">{{ item.TEXT }}</router-link>
+        </li>
+        <li>
+          <button class="bg-blue-600 text-white border rounded-xl px-3 py-1 active:bg-blue-800">
+            {{ HEADER.LOGIN }}
+          </button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { HEADER } from '../constants/String';
+import HamburgerMenu from '../components/icons/HamburgerMenu.vue';
+import { ref } from 'vue';
+
+const viewMenu = ref(false);
+
+const openMenu = () => {
+  viewMenu.value = !viewMenu.value;
+};
 </script>
 
-<style scoped></style>
+<style lang="postcss" scoped>
+.menu {
+  @apply hidden lg:flex;
+}
+
+.hamburger_menu {
+  @apply lg:hidden flex flex-row gap-4 h-full;
+}
+
+.hamburger_menu > .items {
+  @apply absolute flex flex-col items-center bg-white translate-y-12;
+}
+
+.hamburger_menu > .items > li {
+  @apply w-full py-2 px-3 border border-gray-400;
+}
+</style>
