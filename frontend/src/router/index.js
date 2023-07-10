2c531281519e9bcb27483bd1d0fd83098e04c7b7
han0224 <dbn0224@gmail.com>
2023-07-10 17:50:04 +0900

    feat: 라우터 변경
    children을 이용해 공통 레이아웃 설정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import DefaultLayout from '../layouts/DefaultLayout.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'defaultLayout',
+      component: DefaultLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView
+        }
+      ]
     }
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // }
     // import 다른 버전
     // {
     //   path: '/about',
