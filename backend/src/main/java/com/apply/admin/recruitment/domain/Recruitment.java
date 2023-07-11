d3b934b8f5cba1f4bbb7157be710bc7c62f15106
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-11 22:19:00 +0900

    refactor(domain): 모집에 대한 엔티티 설계 수정
    - id Long 타입으로 수정
    - 커리큘럼 이미지 변수명 수정
    - Getter 메서드 선언

--- backend/src/main/java/com/apply/admin/recruitment/domain/Recruitment.java
+++ backend/src/main/java/com/apply/admin/recruitment/domain/Recruitment.java
@@ -15,7 +15,7 @@ public class Recruitment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idx")
-    private int idx;
+    private Long idx;
 
     @Column(name = "course", nullable = false, length = 10)
     private String course;
@@ -30,8 +30,8 @@ public class Recruitment extends BaseEntity {
     @Column(name = "recruitment_status", nullable = false)
     private RecruitmentStatus recruitmentStatus;
 
-    @Column(name = "profile_image_url", nullable = false)
-    private String profileImageUrl;
+    @Column(name = "curriculumImage", nullable = false, length = 100)
+    private String curriculumImage;
 
     @CreatedDate
     @Column(name = "start_date", nullable = false)
@@ -45,14 +45,50 @@ public class Recruitment extends BaseEntity {
     }
 
     @Builder
-    public Recruitment(int idx, String course, int contentMaxSize, int headCount, RecruitmentStatus recruitmentStatus, String profileImageUrl, LocalDateTime startDate, LocalDateTime dueDate, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    public Recruitment(Long idx, String course, int contentMaxSize, int headCount, RecruitmentStatus recruitmentStatus, String curriculumImage, LocalDateTime startDate, LocalDateTime dueDate) {
         this.idx = idx;
         this.course = course;
         this.contentMaxSize = contentMaxSize;
         this.headCount = headCount;
         this.recruitmentStatus = recruitmentStatus;
-        this.profileImageUrl = profileImageUrl;
+        this.curriculumImage = curriculumImage;
         this.startDate = startDate;
         this.dueDate = dueDate;
+    }
+
+    public void change(final String course) {
+        this.course = course;
+    }
+
+    public Long getIdx() {
+        return idx;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public int getContentMaxSize() {
+        return contentMaxSize;
+    }
+
+    public int getHeadCount() {
+        return headCount;
+    }
+
+    public RecruitmentStatus getRecruitmentStatus() {
+        return recruitmentStatus;
+    }
+
+    public String getCurriculumImage() {
+        return curriculumImage;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
     }
 }
